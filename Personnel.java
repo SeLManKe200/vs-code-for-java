bd13a97c451ea57353aec787c9d63a15a3d5bc61
ahmet selman <karisanselman@gmail.com>
2022-04-20 17:53:27 +0300

    a

--- Personnel.java
+++ Personnel.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Personnel extends People {
     private ArrayList<Personnel> personnels = new ArrayList<>();
     public static int numberOfPersonnels;
+    
 
     public Personnel(int age, String name, String surname, double weight, double height, String TC) {
         super(age, name, surname, weight, height, TC);
