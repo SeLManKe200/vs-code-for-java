99b0bc7e800703793daf8d111c5f12386b161fc7
Feyzullah Asıllıoğlu <feyzullahasillioglu@gmail.com>
2022-04-21 15:06:22 +0300

    a

--- Doctor.java
+++ Doctor.java
@@ -1,32 +1,22 @@
 public class Doctor extends Person {
     private double salary;
-    private int id;
     private Hospital hospital;
     public static int numberOfPersonnels;
 
-    public Doctor(int age, String firstName, String lastName, String TC, String gender, double salary, int id,
+    public Doctor(int age, String firstName, String lastName, String TC, String gender, double salary,
             Hospital hospital) {
         super(age, firstName, lastName, TC, gender);
         this.salary = salary;
         this.hospital = hospital;
-        this.id = id;
         numberOfPersonnels++;
     }
-    
+
     public double getSalary() {
         return this.salary;
     }
 
     public void setSalary(double salary) {
         this.salary = salary;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public Hospital getHospital() {
@@ -38,4 +28,3 @@ public class Doctor extends Person {
     }
 
 }
- 
--- Test.java
+++ Test.java
@@ -1,31 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
+        ArrayList<Doctor> doctors = new ArrayList<>();
         Hospital hospital1 = new Hospital("location", "Bezmialem");
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Hasta girişi için 1'i tuşlayınız \n Doktor girişi için 2'i tuşlayınız \n");
-        System.out.println("Eğer kaydınız yoksa 0'a basınız");
+        System.out.print("Hasta girişi için 1'i tuşlayınız \nDoktor girişi için 2'i tuşlayınız \n");
+        System.out.print("Eğer kaydınız yoksa 0'a basınız\n");
         int a = scan.nextInt();
         switch (a) {
             case 0:
-                System.out.println("Doktor kaydı oluşturmak için 1'e basınız \nHasta kaydı için 2'e basınız");
+                System.out.print("Doktor kaydı oluşturmak için 1'e basınız \nHasta kaydı için 2'e basınız\n");
                 int b = scan.nextInt();
                 switch (b) {
                     case 1:
-                        System.out.println("Yaşınızı giriniz");
+                        System.out.print("Yaşınızı giriniz:");
                         int age = scan.nextInt();
-                        System.out.println("Adınızı giriniz");
-                        String firstName = scan.nextLine();
-                        System.out.println("Soyadınızı giriniz");
-                        String lastName = scan.nextLine();
-                        System.out.println("TC'nizi giriniz");
-                        String TC = scan.nextLine();
-                        System.out.println("Cinsiyetinizi giriniz");
-                        String gender = scan.nextLine();
+                        System.out.print("Adınızı giriniz:");
+                        String firstName = scan.next();
+                        System.out.print("Soyadınızı giriniz:");
+                        String lastName = scan.next();
+                        System.out.print("TC'nizi giriniz (11 haneli):");
+                        String TC = scan.next();
+                        System.out.print("Cinsiyetinizi giriniz (Erkek, Kadın):");
+                        String gender = scan.next();
 
-                        Person doctor1 = new Doctor(age, firstName, lastName, TC, gender, 31000, 1, hospital1);
+                        doctors.add(new Doctor(age, firstName, lastName, TC, gender, 31000, hospital1));
 
                     case 2:
 
